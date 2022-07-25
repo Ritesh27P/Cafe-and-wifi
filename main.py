ac0a2de40fe82a4b4d16d9d750cec75fe89a633e
ritesh-blog27 <riteshminecraft27@gmail.com>
2022-07-25 15:23:01 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafe.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Cafe(db.Model):
+    __tablename__ = 'cafe'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    address = db.Column(db.String(100), unique=True, nullable=False)
+    url = db.Column(db.String(200), unique=True, nullable=True)
+    wifi_rating = db.Column(db.String(20), unique=True, nullable=False)
+    power = db.Column(db.String(20), unique=True, nullable=False)
+
+db.create_all()
+
